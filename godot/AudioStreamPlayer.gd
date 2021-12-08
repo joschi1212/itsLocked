cad93c2f2d856de7daeef2cd06f1ffb09a6b2e88
joschi1212 <e1427350@student.tuwien.ac.at>
2021-12-08 21:09:26 +0100

    all voices added!

--- godot/AudioStreamPlayer.gd
+++ godot/AudioStreamPlayer.gd
@@ -1,6 +1,9 @@
 extends AudioStreamPlayer
 
-export(Array, AudioStream) var audio_files: Array
+export(Array, AudioStream) var audio_files1: Array
+export(Array, AudioStream) var audio_files2: Array
+export(Array, AudioStream) var audio_files3: Array
+
 export var interrupting: bool
 
 # Called when the node enters the scene tree for the first time.
@@ -8,7 +11,19 @@ func _ready():
 	randomize()
 	pass # Replace with function body.
 
-func play_random() -> void:
+func play_specific(i) ->void:
+	stop()
+	stream = audio_files1[i]
+	play()
+
+func play_random(par) -> void:
+	var audio_files: Array
+	if(par > 1000):
+		audio_files = audio_files2
+	else:
+		audio_files = audio_files1
+	if(audio_files2.size() == 0):
+		audio_files = audio_files1
 	if(interrupting):
 		var random_index: = randi() % audio_files.size()
 		stop()
--- godot/RigidBody2D.gd
+++ godot/RigidBody2D.gd
@@ -5,16 +5,24 @@ signal clicked
 var hand_open = load("res://assets/cursor_open.png")
 var hand_closed = load("res://assets/cursor_closed.png")
 
-var random_wood_player
-var random_voice_player
+var image
+var chest_closed = load("res://assets/chest_closed.png")
+var chest_open = load("res://assets/chest_open.png")
 
+var wood_player
+var voice_player
+var specific_voice_sfx
 
 var held = false
+var broken = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	random_wood_player = get_node("../random_wood_sfx")
-	random_voice_player = get_node("../random_voice_sfx")
+	wood_player = get_node("../random_wood_sfx")
+	voice_player = get_node("../random_voice_sfx")
+	specific_voice_sfx = get_node("../specific_voice_sfx")
+
+	image = get_node("Sprite")
 	connect("body_entered", self, "_on_body_entered")
 	pass # Replace with function body.
 
@@ -35,9 +43,19 @@ func _input_event(viewport, event, shape_idx):
 func _on_body_entered(body):
 	var force = get_linear_velocity().length()
 	print(force)
-	if(force > 100):
-		random_wood_player.play_random()
-		random_voice_player.play_random()
+
+	if(force > 100 and force < 2000 and !broken):
+		wood_player.play_random(force)
+		voice_player.play_random(force)
+	if(force > 2000):
+		broken = true
+		image.set_texture(chest_open)
+		specific_voice_sfx.play_specific(0)
+		yield(get_tree().create_timer(5, false), "timeout")
+		image.set_texture(chest_closed)
+		specific_voice_sfx.play_specific(1)
+		broken = false
+	
 
 
 
